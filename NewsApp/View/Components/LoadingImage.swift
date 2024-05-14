f440875e091a8fe1aa9cba0a85c4d2273e795c9f
dantoso <80010223+dantoso@users.noreply.github.com>
2024-05-14 14:45:02 -0300

    feat: adding loading image component view

---
+++ NewsApp/View/Components/LoadingImage.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct LoadingImage: View {
+	let hasURL: Bool
+	let image: UIImage?
+
+	var body: some View {
+		Group {
+			if hasURL {
+				if let image {
+					Image(uiImage: image)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.mask {
+							RoundedRectangle(cornerRadius: 24)
+						}
+						.padding(.bottom, 56)
+				} else {
+					ProgressView()
+				}
+			}
+		}
+	}
+}
